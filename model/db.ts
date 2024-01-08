545667efa0d269ce74eb1e994f3d420194794a40
nedim begic <nedim.begic93@gmail.com>
2024-01-08 22:47:54 +0100

    adding .env

--- model/db.ts
+++ model/db.ts
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  database: "gameshop",
-  password: "nodemysql",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
 });
 
 module.exports = pool.promise();
--- server.ts
+++ server.ts
@@ -2,17 +2,16 @@ import express from "express";
 import * as dotenv from "dotenv";
 import games from "./routes/games";
 import errorHandler = require("./middleware/error");
-const db = require("./model/db");
 import multer from "multer";
 
 // Configuration
 dotenv.config();
 const app = express();
 // Multer configuration
-const storage = multer.memoryStorage(); // Using memory storage for simplicity
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
-app.use(upload.single("file")); // Multer middleware for handling file uploads
-app.use(express.urlencoded({ extended: true })); // Middleware for parsing text fields
+app.use(upload.single("file"));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // routes
@@ -21,6 +20,6 @@ app.use("/games", games);
 // middleware
 app.use(errorHandler);
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server is running");
 });

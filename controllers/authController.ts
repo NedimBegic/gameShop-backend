2ee0c5906e1ab51e1a7c6cb834f8e641864b2aa2
nedim begic <nedim.begic93@gmail.com>
2024-01-14 19:52:28 +0100

    making functionality for registering the user to database

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -4,12 +4,14 @@ import ErrorResponse from "../utils/errorResponse";
 import { RegisteringUser, User } from "../utils/types";
 import uploadToImgur from "../model/imgur";
 import { hashPassword } from "../utils/encrypt";
+import { isNickNameTaken, isEmailTaken, saveNewUser } from "../model/auth";
 
 // ROUTE: /register,
 // METHOD POST,
 // DESC: register an account
 export const registerController = asyncHandler(
   async (req: Request, res: Response, next) => {
+    /* Entered data from user */
     const { nickName, email, password, confirmPassword } = req.body;
     const registerData: RegisteringUser = {
       nickName,
@@ -18,9 +20,24 @@ export const registerController = asyncHandler(
       confirmPassword,
     };
     const fileImage: Express.Multer.File | undefined = req.file;
-    // Verify the email ??????????? <-----------------------------------------|
+    // Verify the email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(registerData.email)) {
+      return next(new ErrorResponse("Invalid email format", 400));
+    }
+    // Verify password match
     if (registerData.password != registerData.confirmPassword) {
       return next(new ErrorResponse("The passwords don't match", 404));
+    }
+    // Check if the user nickName already exists
+    let nickNameTaken = await isNickNameTaken(nickName);
+    if (nickNameTaken) {
+      return next(new ErrorResponse("Nickname already taken.", 404));
+    }
+    // Check if the user email already exist
+    let emailTaken = await isEmailTaken(email);
+    if (emailTaken) {
+      return next(new ErrorResponse("Email already exist", 404));
     }
     // for storing user image
     let userImageUrl: string;
@@ -37,7 +54,12 @@ export const registerController = asyncHandler(
       password: hashedPassword,
       userImageUrl,
     };
-    res.json({ success: true, data: newUser });
+    const saveUser = await saveNewUser(newUser);
+    // check if user is saved
+    if (!saveUser.success) {
+      return next(new ErrorResponse(saveUser.message, 404));
+    }
+    res.json({ success: true, message: "User succesfully saved in database" });
   }
 );
 // Route: /login,
---
+++ model/auth.ts
@@ -0,0 +1,48 @@
+const db = require("../model/db");
+import { User } from "../utils/types";
+
+/* +++ Save a new registred user to database */
+const saveNewUser = async (userData: User): Promise<any> => {
+  const { nickName, email, password, userImageUrl } = userData;
+  let sql =
+    "INSERT INTO users (nickName, email, password, userImageUrl) VALUES (?, ?, ?, ?)";
+  try {
+    const [rows] = await db.execute(sql, [
+      nickName,
+      email,
+      password,
+      userImageUrl,
+    ]);
+    return { success: true, data: rows };
+  } catch (err) {
+    console.log(err);
+    return { success: false, msessage: err };
+  }
+};
+/* +++ Check if there is a user with that nickName */
+const isNickNameTaken = async (nickName: string): Promise<boolean> => {
+  try {
+    let sql = "SELECT * FROM users WHERE nickName = ?";
+    const [rows] = await db.execute(sql, [nickName]);
+    // if the array is empty nickName is not taken
+    return rows.length === 0 ? false : true;
+  } catch (error) {
+    console.error("Error checking nickname:", error);
+    throw error; // Handle the error appropriately in your application
+  }
+};
+
+/* +++ Check if there is a user with that email */
+const isEmailTaken = async (email: string): Promise<boolean> => {
+  try {
+    let sql = "SELECT * FROM users WHERE email = ?";
+    const [rows] = await db.execute(sql, [email]);
+    // if the array is empty email is not taken
+    return rows.length === 0 ? false : true;
+  } catch (error) {
+    console.error("Error checking email:", error);
+    throw error; // Handle the error appropriately in your application
+  }
+};
+
+export { isNickNameTaken, isEmailTaken, saveNewUser };
